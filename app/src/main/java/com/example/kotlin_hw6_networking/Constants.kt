e4d2a8dcf71d792a7da86e46824e8da81434eaa0
Hrihoriy <hryhoriy.kysil@chisw.com>
2022-10-20 20:31:13 +0300

    rework app to use only 1 thread

---
+++ app/src/main/java/com/example/kotlin_hw6_networking/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin_hw6_networking
+
+class Constants {
+    companion object{
+        val urlAddress:String = "https://zoo-animal-api.herokuapp.com/animals/rand/"
+    }
+}
--- app/src/main/java/com/example/kotlin_hw6_networking/fragments/RecyclerFragment.kt
+++ app/src/main/java/com/example/kotlin_hw6_networking/fragments/RecyclerFragment.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin_hw6_networking.Animal
+import com.example.kotlin_hw6_networking.Constants
 import com.example.kotlin_hw6_networking.R
 import com.example.kotlin_hw6_networking.services.RetrofitEndpointInterface
 import com.google.gson.Gson
@@ -22,83 +23,73 @@ import java.net.URL
 
 class RecyclerFragment : Fragment() {
 
-    var urlString: String = "https://zoo-animal-api.herokuapp.com/animals/rand/"
     val handler = Handler(Looper.getMainLooper())
     var gson = Gson()
     var okHttpClient: OkHttpClient = OkHttpClient()
     var animalAdapter = AnimalAdapter()
 
-    fun performRetrofitRequest(sUrl:String){
-        var httpThread = Thread(Runnable {
-            //Log.d("ASD","Thread "+Thread.currentThread().name+" STARTED")
-            val retrofitClient = Retrofit.Builder()
-                .baseUrl(sUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            var myInterface = retrofitClient.create(RetrofitEndpointInterface::class.java)
-            var call = myInterface.getUser("3")
-            var result = call?.execute()
-            var resultBody = result?.body()
-            synchronized(animalAdapter){
-                if (resultBody!= null) {
-                    animalAdapter.addData(resultBody)
-                    handler.post {
-                        animalAdapter.notifyItemChanged(animalAdapter.itemCount)
-                    }
-                }
-            }
-        })
-
-        httpThread.start()
+    fun performRetrofitRequest(sUrl: String):Array<Animal>? {
+        //Log.d("ASD","Thread "+Thread.currentThread().name+" STARTED")
+        val retrofitClient = Retrofit.Builder()
+            .baseUrl(sUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        var myInterface = retrofitClient.create(RetrofitEndpointInterface::class.java)
+        var call = myInterface.getUser("3")
+        var result = call?.execute()
+        return result?.body()
     }
 
-    private fun getOkhttpRequest(sUrl: String): String? {
-        var result: String? = null
+    private fun performOkhttpRequest(sUrl: String): Array<Animal>? {
+        var result: Array<Animal>? = null
         try {
             val url = URL(sUrl)
             val request = Request.Builder().url(url).build()
             val response = okHttpClient.newCall(request).execute()
-            result = response.body?.string()
+
+            result = gson.fromJson(response.body?.string(), Array<Animal>::class.java)
         } catch (err: Error) {
             print("Error when executing get request: " + err.localizedMessage)
         }
         return result
     }
 
-    fun performOkhhtpRequest() {
-        var httpThread = Thread(Runnable {
-            //Log.d("ASD", "okHttp thread start working")
-            var jsonResponse = getOkhttpRequest(urlString + "6")
-            //Log.d("ASD", jsonResponse.toString())
-
-            var animals = gson.fromJson(jsonResponse, Array<Animal>::class.java)
-            synchronized(animalAdapter){
-                animalAdapter.addData(animals)
-                handler.post {
-                    animalAdapter.notifyItemChanged(animalAdapter.itemCount)
-                }
+    fun launchRequests() {
+        var thread = Thread(Runnable {
+            var resultList = mutableListOf<Animal>()
+            var firstGroup = performOkhttpRequest(Constants.urlAddress+"6")
+            Log.d("ASD","first finished")
+            var secondGroup = performRetrofitRequest(Constants.urlAddress)
+            Log.d("ASD","second finished")
+            if (firstGroup != null){
+                resultList.addAll(firstGroup)
+            }
+            if(secondGroup != null){
+                resultList.addAll(secondGroup)
+            }
+            handler.post {
+                animalAdapter.addData(resultList.toTypedArray())
+                animalAdapter.notifyItemChanged(0)
             }
         })
-
-        httpThread.start()
+        thread.start()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("ASD","Fragment on create")
+        Log.d("ASD", "Fragment on create")
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        Log.d("ASD","Fragment on create view")
+        Log.d("ASD", "Fragment on create view")
         return inflater.inflate(R.layout.fragment_recycler, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        Log.d("ASD","Fragment on view created")
-        performOkhhtpRequest()
-        performRetrofitRequest(urlString)
+        Log.d("ASD", "Fragment on view created")
+        launchRequests()
         var recycleView = view.findViewById<RecyclerView>(R.id.recycleView)
         recycleView.layoutManager = LinearLayoutManager(context)
         recycleView.adapter = animalAdapter
